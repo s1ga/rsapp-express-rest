80a33e6ffc1fa887943bd50f667ce2b9f79ade22
Sergey Harlanov <s.v.harlanov@gmail.com>
2020-10-06 22:57:32 +0300

    feat: creat routes for User

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
+// requiring dependencies
 const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
+
+// require routes
 const userRouter = require('./routes/user.router');
 
+// create express app
 const app = express();
+
+// swagger api
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
+// using json
 app.use(express.json());
 
+// using swagger doc
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
+// base url path
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
     res.send('Service is running!');
@@ -19,6 +28,7 @@ app.use('/', (req, res, next) => {
   next();
 });
 
+// using routes
 app.use('/users', userRouter);
 
 module.exports = app;
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,10 +2,26 @@ const router = require('express').Router();
 const User = require('../models/user.model');
 const usersService = require('../services/user.service');
 
-router.route('/').get(async (req, res) => {
+router.get('/', async (req, res) => {
   const users = await usersService.getAll();
   // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  res.status(200).json(users.map(User.toResponse));
 });
+
+// router.get('/:id', (req, res) => {
+
+// });
+
+// router.post('/', (req, res) => {
+
+// });
+
+// router.put('/:id', (req, res) => {
+
+// });
+
+// router.delete('/:id', (req, res) => {
+
+// });
 
 module.exports = router;
